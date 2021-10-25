7a8e043c5af1ae1f165706684e5204b56f3a4c3a
simplyDonald <donaldabuah@gmail.com>
2021-10-25 23:53:35 +0000

    worked on tail function

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🏁🏁🏁Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const head = function(array){
+  if (array){
+  return array[0];
+  } else{
+    return undefined;
+  }
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+
---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🏁🏁🏁Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const tail = function(array) {
+  if (array) {
+    return array.filter((elem) =>{
+      return elem !== array[0];
+    });
+  } else {
+    return undefined;
+  }
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
