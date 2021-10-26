4b4233176d543e2cc5e43a52faf2f55a8d8de7b7
simplyDonald <donaldabuah@gmail.com>
2021-10-26 13:37:45 +0000

    added reverse.js code for reversing strings passed as args

---
+++ reverse.js
@@ -0,0 +1,14 @@
+const numberCast = function(array) {
+  let newargs = array.slice(2);
+  // const newArray = [];
+  const newWords = newargs.map((elem)=>{
+    let newWord = '';
+    for (let i = elem.length - 1; i >= 0; i--) {
+      newWord += elem[i];
+    }
+    return newWord;
+  })
+  return newWords.join(' ')
+};
+
+console.log(numberCast(process.argv));
