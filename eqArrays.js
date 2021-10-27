7b73c7320a5fab55bf02cf5759a797bdc74434fe
simplyDonald <donaldabuah@gmail.com>
2021-10-27 01:28:50 +0000

    tweaked the function to fix errors on incorrect outputs when using in assertArraysEquals

--- eqArrays.js
+++ eqArrays.js
@@ -11,19 +11,15 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        return true;
-      } else {
-        return false;
-      }
-  
-    }
-  } else {
+  if (array1.length !== array2.length) {
     return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+  return true;
 
 };
 
