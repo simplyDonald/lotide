b71345bbac38ffe011dc04b5041429550e9ce53a
simplyDonald <donaldabuah@gmail.com>
2021-10-27 22:58:54 +0000

    took care of the count only function

--- middle.js
+++ middle.js
@@ -32,7 +32,7 @@ const middle = function(array) {
   return midArray;
 }
 
-console.log(middle([1,2])) // => [2]
+console.log(middle([1,2,3,4,5])) // => [2]
 console.log(middle([1, 2, 3, 4])) // => [2]
 
 
