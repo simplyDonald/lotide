67bfb9b6d444562fd3fc802928cbf9daf423ff53
simplyDonald <donaldabuah@gmail.com>
2021-10-27 01:24:05 +0000

    created a function that made use of a previous one used to compare arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  }
+  return true;
+
+};
+// console.log(eqArrays([1,22,3],[1,22,65]));
+const assertArraysEqual = function(array1, array2){
+  console.log(eqArrays(array1, array2));
+}
+
+assertArraysEqual([1,2],[1,4]);
