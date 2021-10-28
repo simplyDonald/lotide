16e7c5e47f34d276625cc131b8ae4b02fd13b0fb
simplyDonald <donaldabuah@gmail.com>
2021-10-28 01:18:17 +0000

    fixed the length end case

---
+++ eqObjects.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🏁🏁🏁Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const keysObj1 = Object.keys(object1);
+  const keysObj2 = Object.keys(object2);
+  if (keysObj1.length !== keysObj2.length) {
+    return false;
+  }
+  for (let i = 0; i < keysObj1.length; i++) {
+    if (keysObj1[i] !== keysObj2[i]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba),true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc),false); // => false
