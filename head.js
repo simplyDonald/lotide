0c9c490b768e1d218c3c06edf0dbb750cbef48f7
simplyDonald <donaldabuah@gmail.com>
2021-11-01 17:37:18 +0000

    refactored code with modular exports and imports

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔🚫⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array){
   if (array){
@@ -15,8 +8,6 @@ const head = function(array){
   }
 };
 
+module.exports = head;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
 
